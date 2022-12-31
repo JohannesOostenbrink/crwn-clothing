b52ab2790caf12936984a2ee20c691f555af6bd7
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-31 11:55:59 +0700

    setting up categories

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 
 
 const App = () => {
+
+  const categories = [
+
+    {
+      title: 'Hats',
+      id: 1,
+    
+    },
+
+    {
+      title: 'Jackets',
+      id: 2,
+    
+    },
+
+    {
+      title: 'Sneakers',
+      id: 3,
+    
+    },
+
+    {
+      title: 'Womens',
+      id: 4,
+    
+    },
+
+    {
+      title: 'Mens',
+      id: 5,
+    
+    },
+
+  ]
+
   return (
     <div className="categories-container">
+      {categories.map(({title}) => (
 
-      <div className = "category-container">
+        <div className = "category-container">
         <img />
         <div className = "category-body-container">
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
-      </div>
+        </div>
 
-      <div className = "category-container">
-        <img />
-        <div className = "category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
 
-      <div className = "category-container">
-        <img />
-        <div className = "category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className = "category-container">
-        <img />
-        <div className = "category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className = "category-container">
-        <img />
-        <div className = "category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      
-    
     </div>
   );
 }
