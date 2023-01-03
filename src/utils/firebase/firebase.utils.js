1c8c8ae7efd5bc1b22469b4c114ed200284a6927
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-03 19:37:32 +0700

    Firebase Auth

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import {initializeApp} from 'firebase/app';
+import {
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider
+} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfhONhu9DQtEjSiAz3m5mltUNPeM9ZrMw",
+    authDomain: "crwn-clothing-123f4.firebaseapp.com",
+    projectId: "crwn-clothing-123f4",
+    storageBucket: "crwn-clothing-123f4.appspot.com",
+    messagingSenderId: "839282771951",
+    appId: "1:839282771951:web:e8dffc4db5d0a2d20b581a"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
