cdbc74254bd0a29e471c6fa885ada69d3ede8427
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-10 19:48:10 +0700

    more tidy up work

--- src/routes/authentication/authentication.component.js
+++ src/routes/authentication/authentication.component.js
@@ -1,26 +1,25 @@
-// import {useEffect} from 'react';
 import SignInForm from '../../Components/sign-in-form/sign-in-form.component';
-// import {getRedirectResult} from 'firebase/auth'
+import SignUpForm from '../../Components/sign-up/sign-up-form.component';
 import './authentication.component.styles.scss'
 
-import SignUpForm from '../../Components/sign-up/sign-up-form.component';
-
 const Authentication = () => {
-    // useEffect(async () => {
-    //    const response = await getRedirectResult(auth);
-    //    if(response) {
-    //     const userDocRef = await createUserDocumentFromAuth(response.user);
-    //    }
-    // }, []);
-
     return(
         <div className = 'authentication-container'>
             <SignInForm/>,
-            {/* <button onClick = {logGoogleUser}>Sign in with Google</button> */}
-            {/* <button onClick = {signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
             <SignUpForm />
         </div>
     )
 }
 
 export default Authentication;
+
+// import {useEffect} from 'react';
+// import {getRedirectResult} from 'firebase/auth'
+  // useEffect(async () => {
+    //    const response = await getRedirectResult(auth);
+    //    if(response) {
+    //     const userDocRef = await createUserDocumentFromAuth(response.user);
+    //    }
+    // }, []);
+      {/* <button onClick = {logGoogleUser}>Sign in with Google</button> */}
+    {/* <button onClick = {signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
