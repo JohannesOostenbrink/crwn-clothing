f0f486fe6537c3f131c080a67aa94f152d70a10a
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-18 19:54:04 +0700

    minor fix

--- src/Components/product-card/product-card.component.js
+++ src/Components/product-card/product-card.component.js
@@ -5,11 +5,11 @@ import Button from '../button/button.component'
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product
     return(
-        <div className = 'Product-Card-Container'>
+        <div className = 'product-card-container'>
                 <img src={imageUrl} alt={`${name}`} />
-                <div className = 'Footer' >
+                <div className = 'footer' >
                     <span className='name'>{name}</span>
-                    <span className='Price'>{price}</span>
+                    <span className='price'>{price}</span>
                 </div>
                 <Button buttonType='inverted'>Add to Cart</Button>
             </div>
