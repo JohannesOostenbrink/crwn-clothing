f73fb3994dd11143f2882a005aa7fcd3eb4ba579
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-19 20:21:48 +0700

    Cart Context

--- src/Components/cart-dropdown/cart-dropdown.component.js
+++ src/Components/cart-dropdown/cart-dropdown.component.js
@@ -1,12 +1,17 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component';
+import { CartContext } from '../../context/cart.context';
+import { useContext } from 'react';
 
 const CartDropdown = () => {
+    const {isOpen} = useContext(CartContext);
     return (
         <div className='cart-dropdown-container'>
+            {isOpen &&
             <div className='cart-items'>
 
             </div>
+}
             <Button>GO TO CHECKOUT</Button>
 
         </div>
