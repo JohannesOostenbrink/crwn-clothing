4e7181e91212fd7bb8461b3b836cb813e50d6c26
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-20 19:59:51 +0700

    Add to cart Pt.1

--- src/context/cart.context.js
+++ src/context/cart.context.js
@@ -1,13 +1,34 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.product.id === productToAdd.id
+  );
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.product.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { product: productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
     isOpen: false,
     toggleDropdown: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
   });
 
 export const CartProvider = ({ children }) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleDropdown = () => setIsOpen(!isOpen);
+    const [cartItems, setCartItems] = useState([])
+
+    const addItemToCart = (productToAdd) => {
+      setCartItems(addCartItem(cartItems,productToAdd));
+    }
   
     return (
       <CartContext.Provider value={{ isOpen, toggleDropdown }}>
