9219aff426dbc25cf6a87c97d3d789e8ce2222ed
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-20 20:00:03 +0700

    Add to cart pt1

--- src/Components/cart-dropdown/cart-dropdown.component.js
+++ src/Components/cart-dropdown/cart-dropdown.component.js
@@ -2,6 +2,7 @@ import './cart-dropdown.styles.scss'
 import Button from '../button/button.component';
 import { CartContext } from '../../context/cart.context';
 import { useContext } from 'react';
+import CartItem from '.././cart-item/cart-item.component'
 
 const CartDropdown = () => {
     const {isOpen} = useContext(CartContext);
@@ -9,6 +10,7 @@ const CartDropdown = () => {
         <div className='cart-dropdown-container'>
             {isOpen &&
             <div className='cart-items'>
+                {[].map(item => <CartItem item={item} />)}
 
             </div>
 }
