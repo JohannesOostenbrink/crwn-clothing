31f40765467d26b4aa20d11e8c29eef89b790f3c
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-25 20:00:41 +0700

    cart item pt3

--- src/Components/checkout/checkout-item.component.js
+++ src/Components/checkout/checkout-item.component.js
@@ -1,7 +1,13 @@
 import './checkout-item.styles.scss'
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 
 const CheckoutItem = ({cartItem}) => {
     const {name,imageUrl,price,quantity} = cartItem;
+
+    const {clearItemFromCart,addItemToCart, removeItemFromCart} = useContext(CartContext);
+    const addItemHandler = () => addItemToCart(cartItem)
+    const removeItemHandler = () => removeItemFromCart(cartItem)
 
     return(
         <div className='checkout-item-container'>
@@ -9,9 +15,14 @@ const CheckoutItem = ({cartItem}) => {
                 <img src={imageUrl} alt={`${name}`}/>
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div onClick={removeItemHandler} className='arrow'>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div onClick={addItemHandler} className='arrow'>&#10095;</div>
+            
+            </span>
             <span className='price'>{price}</span>
-            <div className='remove-button'>
+            <div onClick={()=>clearItemFromCart(cartItem)} className='remove-button'>
                 &#10005;
 
             </div>
