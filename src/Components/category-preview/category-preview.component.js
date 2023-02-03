47038880024abff8ce98430b74b2be8e73bf2fb8
JohannesOostenbrink <oostenhan@gmail.com>
2023-02-03 19:23:19 +0700

    Minor Change

--- src/Components/category-preview/category-preview.component.js
+++ src/Components/category-preview/category-preview.component.js
@@ -21,16 +21,3 @@ const CategoryPreview = ({title, products}) => {
 }
 
 export default CategoryPreview;
-
- {/* </div>
-            {Object.keys(categoriesMap).map(title => (
-            <Fragment key={title}>
-                <h2>{title}</h2>
-                <div className= 'products-container'>
-                    {categoriesMap[title].map(product => (
-                    <ProductCard key={product.id} product={product} />
-                    ))}
-                </div>
-            </Fragment>
-            ))}
-        </Fragment> */}
